53f98885135997e0b17e04eda1fe752f433bb260
mickykebe <mickykebe@gmail.com>
2013-03-11 15:47:04 +0300

    tree

---
+++ chapter2/tree.scm
@@ -0,0 +1,5 @@
+(define (count-leaves x)
+    (cond   ((null? x) 0)
+            ((not (pair? x)) 1)
+            (else (+    (count-leaves (car x))
+                        (count-leaves (cdr x))))))
