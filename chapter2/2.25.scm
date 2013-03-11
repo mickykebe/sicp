94e3b3dcb3ef1a0749cdb4421636522d2149a5d3
mickykebe <mickykebe@gmail.com>
2013-03-11 16:38:12 +0300

    2.25

---
+++ chapter2/2.25.scm
@@ -0,0 +1,10 @@
+(1 3 (5 7) 9)
+; (car (cdr (car (cdr (cdr [items])))))
+
+((7))
+
+; (car (car [items]))
+
+(1 (2 (3 (4 (5 (6 7))))))
+
+;(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr list-3))))))))))))
