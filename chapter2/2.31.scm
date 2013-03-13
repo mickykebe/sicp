07c08d8fa42268231ff7753e4924e2c767670d16
mickykebe <mickykebe@gmail.com>
2013-03-13 10:20:58 +0300

    2.31

---
+++ chapter2/2.31.scm
@@ -0,0 +1,5 @@
+(define (tree-map proc tree)
+    (cond   ((null? tree) tree)
+            ((not (pair? tree)) (proc tree))
+            (else (cons (tree-map proc (car tree))
+                        (tree-map proc (cdr tree))))))
