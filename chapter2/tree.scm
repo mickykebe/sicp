19a517bdb14482e298897271dabb50a9df16d7fb
mickykebe <mickykebe@gmail.com>
2013-03-13 09:43:42 +0300

    tree updated

--- chapter2/tree.scm
+++ chapter2/tree.scm
@@ -3,3 +3,16 @@
             ((not (pair? x)) 1)
             (else (+    (count-leaves (car x))
                         (count-leaves (cdr x))))))
+
+(define (scale-tree tree factor)
+    (cond   ((null? tree) tree)
+            ((not (pair? tree)) (* tree factor))
+            (else (cons (scale-tree (car tree) factor)
+                        (scale-tree (cdr tree) factor)))))
+
+
+(define (scale-tree tree factor)
+    (map (lambda (sub-tree)
+            (if (pair? sub-tree)
+                (scale-tree sub-tree factor)
+                (* sub-tree factor))) tree))
