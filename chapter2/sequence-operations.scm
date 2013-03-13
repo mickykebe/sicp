fb574709e2348a17b1d5ca7fa3db85455de86d5d
mickykebe <mickykebe@gmail.com>
2013-03-13 16:51:15 +0300

    sequence-operations

---
+++ chapter2/sequence-operations.scm
@@ -0,0 +1,46 @@
+(define nil ())
+
+(define (map proc items)
+    (if (null? items)
+        items
+        (cons (proc (car items)) (map proc (cdr items)))))
+
+(define (filter predicate sequence)
+    (cond   ((null? sequence) sequence)
+            ((predicate (car sequence)) (cons (car sequence) (filter predicate (cdr sequence))))
+            (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+    (if (null? sequence)
+        initial
+        (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+    (if (> low high)
+        nil
+        (cons low (enumerate-interval (+ low 1) high)))
+
+(define (enumerate-tree tree)
+    (cond   ((null? tree) nil)
+            ((not (pair? tree)) (list tree))
+            (else (append   (enumerate-tree (car tree))
+                            (enumerate-tree (cdr tree))))))
+
+(define (sum-odd-squares tree)
+    (accumulate + 
+                0
+                (map square 
+                     (filter odd? 
+                             (enumerate-tree tree)))))
+;(define (fib n)
+;    (if (< (- n 1) 1)
+;        1
+;        (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (even-fibs n)
+    (accumulate cons
+                nil
+                (filter even?
+                        (map fib
+                             (enumerate-interval 0 n)))))
+    
