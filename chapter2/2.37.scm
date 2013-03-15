8cb8ffb029c534f03c9f4c59a0e4d8a25977ba78
mickykebe <mickykebe@gmail.com>
2013-03-15 10:13:31 +0300

    2.37

---
+++ chapter2/2.37.scm
@@ -0,0 +1,12 @@
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (x) (dot-product v x)) m))
+
+(define (transpose mat)
+  (accumulate-n cons nil mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (x) (matrix-*-vector cols x)) m)))
