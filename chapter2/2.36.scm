e9eae309c1fd4487747057fab132fc89c74d560e
mickykebe <mickykebe@gmail.com>
2013-03-15 08:20:03 +0300

    2.36

---
+++ chapter2/2.36.scm
@@ -0,0 +1,7 @@
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map (lambda (x) (car x)) seqs))
+            (accumulate-n op init (map (lambda (x) (cdr x)) seqs)))))
+
+
