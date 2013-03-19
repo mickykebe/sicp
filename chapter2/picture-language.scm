4a48db808ad70e917cc692d99f659411209db969
mickykebe <mickykebe@gmail.com>
2013-03-19 15:13:56 +0300

    picture-language updated

--- chapter2/picture-language.scm
+++ chapter2/picture-language.scm
@@ -1,3 +1,7 @@
+(define (flipped-pairs painter)
+    (let ((painter2 (beside painter (flip-vert painter))))
+        (below painter2 painter2)))
+
 (define (right-split painter n)
     (if (= n 0)
         painter
@@ -19,3 +23,19 @@
     (let ((quarter (corner-split painter n)))
         (let ((half (beside (flip-horiz quarter) quarter)))
             (below (flip-vert half) half))))
+
+(define (square-of-four tl tr bl br)
+    (lambda (painter)
+        (let ((top (beside (tl painter) (tr painter)))
+              (bottom (beside (bl painter) (br painter))))
+            (below bottom top))))
+
+(define (flipped-pairs painter)
+    (let ((combine4 (square-of-four identity flip-vert
+                                    identity flip-vert)))
+        (combine4 painter)))
+
+(define (square-limit painter n)
+    (let ((combine4 (square-of-four flip-horiz identity
+                                    rotate180  flip-vert)))
+        (combine4 (corner-split painter n))))
