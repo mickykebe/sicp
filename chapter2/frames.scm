2a3d7af4a49604aecd008320c65d40ae3fa52fc0
mickykebe <mickykebe@gmail.com>
2013-03-19 14:13:54 +0300

    frames

---
+++ chapter2/frames.scm
@@ -0,0 +1,8 @@
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
