99c80cbab9dab49282fe91315e810c959be012ce
mickykebe <mickykebe@gmail.com>
2013-03-22 09:20:05 +0300

    2.54 finished

--- chapter2/2.54.scm
+++ chapter2/2.54.scm
@@ -1,3 +1,12 @@
 (define (equal? elems1 elems2)
-    (cond ((and (null? elems1) (null? elems2)) #t)
-          (
+    (if (and (null? elems1) (null? elems2))
+        #t
+        (let ((next-elem1 (car elems1))
+              (next-elem2 (car elems2)))
+            (cond ((and (pair? next-elem1) (pair? next-elem2)) (equal? next-elem1 next-elem2))
+                  ((and (not (pair? next-elem1)) (not (pair? next-elem2)))
+                        (if (eq? next-elem1 next-elem2)
+                            (equal? (cdr elems1) (cdr elems2))
+                            #f
+                        ))
+                  (else #f)))))
