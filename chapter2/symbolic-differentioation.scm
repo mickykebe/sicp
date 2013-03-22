e913bee903b8522d4f25f1e7c11a1438ee4e7626
mickykebe <mickykebe@gmail.com>
2013-03-22 19:03:27 +0300

    Symbolic Differentiation updated

--- chapter2/symbolic-differentioation.scm
+++ chapter2/symbolic-differentioation.scm
@@ -1,3 +1,8 @@
+(define (exp x n)
+    (if (= n 0)
+        1
+        (* x (exp x (- n 1)))))
+
 (define (=number? exp num)
     (and (number? exp) (= exp num)))
 
@@ -14,10 +19,17 @@
                          (deriv (multiplicand exp) var))
            (make-product (deriv (multiplier exp) var)
                          (multiplicand exp))))
+        ((exponentiation? exp)
+            (let ((e (exponent exp))
+                  (b (base exp)))
+             (make-product (make-product 
+                                e 
+                                (make-exponentiation b (- e 1)))
+                           (deriv b var))))
         (else
          (error "unknown expression type -- DERIV" exp))))
 
-(define (varaible? x) (symbol? x))
+(define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
   (and (variable? v1) (variable? v2) (eq? v1 v2)))
@@ -34,20 +46,40 @@
           ((=number? m2 1) m1)
           ((and (number? m1) (number? m2)) (* m1 m2))
           (else (list '* m1 m2))))
-          
-
-(define (make-product m1 m2) (list '* m1 m2))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
 
 (define (addend s) (cadr s))
 
-(define (augend s) (caddr s))
+(define (augend s)
+    (let ((last-elem (caddr s)))
+        (if (null? (cdddr s))
+            last-elem
+            (make-sum last-elem (augend (cdr s))))))
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
 
 (define (multiplier p) (cadr p))
 
-(define (multiplicand p) (caddr p))
+(define (multiplicand p)
+    (let ((last-elem (caddr p)))
+        (if (null? (cdddr p))
+            last-elem
+            (make-product last-elem (multiplicand (cdr p))))))
+
+(define (exponentiation? exp)
+    (and (pair? exp) (eq? (car exp) '**)))
+
+(define (base exp)
+    (cadr exp))
+
+(define (exponent exp)
+    (caddr exp))
+
+(define (make-exponentiation base exponent)
+    (cond ((=number? base 0) 0)
+          ((=number? exponent 0) 1)
+          ((and (number? base) (number? exponent)) (exp base exponent))
+          (else (list '** base exponent))))
