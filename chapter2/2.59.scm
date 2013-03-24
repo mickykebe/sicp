641fe02b6c791cf40ea19c7afad61f0f601a322f
mickykebe <mickykebe@gmail.com>
2013-03-24 05:24:19 +0300

    2.59

---
+++ chapter2/2.59.scm
@@ -0,0 +1,5 @@
+(define (union-set set1 set2)
+    (cond ((null? set1) set2)
+          ((not (element-of-set? (car set1) set2))
+            (cons (car set1) (union-set (cdr set1) set2)))
+          (else (union-set (cdr set1) set2))))
