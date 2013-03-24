e0dab6400bd9ad3f1a6f66ee66158284614c50e1
mickykebe <mickykebe@gmail.com>
2013-03-24 09:20:37 +0300

    sets as ordered lists

--- chapter2/sets.scm
+++ chapter2/sets.scm
@@ -14,3 +14,23 @@
           ((element-of-set? (car set1) set2)
                 (cons (car set1) (intersection-set (cdr set1) set2)))
           (else (intersection-set (cdr set1) set2))))
+
+; as ordered lists
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
