fced2c6ccb64381a60e7db41971b540a53928c87
mickykebe <mickykebe@gmail.com>
2013-03-24 09:20:52 +0300

    2.61

---
+++ chapter2/2.61.scm
@@ -0,0 +1,5 @@
+(define (adjoin-set x set)
+    (cond ((null? set) (cons x '()))
+          ((equal? x (car set)) set)
+          ((< x (car set)) (cons x set))
+          ((> x (car set)) (cons (car set) (adjoin-set x (cdr set))))))
