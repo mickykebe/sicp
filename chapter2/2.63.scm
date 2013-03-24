d823287a029dec2b6a9605e4595d8c4c6f1e96b8
mickykebe <mickykebe@gmail.com>
2013-03-24 12:50:52 +0300

    2.63

---
+++ chapter2/2.63.scm
@@ -0,0 +1,21 @@
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list (right-branch tree)
+                                          result-list)))))
+  (copy-to-list tree '()))
+
+;a) They always give the same result. 
+;   For all 3 figures in figure set 2.16 both procedures give the result (1 3 5 7 9 11)
+
+;b) Both procedures grow at a rate of O(N)
