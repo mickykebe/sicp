ee80075c5bd4f626db5ce1e83df1bcf67fe98a4a
mickykebe <mickykebe@gmail.com>
2013-03-25 10:57:52 +0300

    2.67

---
+++ chapter2/2.67.scm
@@ -0,0 +1,11 @@
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+;Answer
+;(decode sample-message sample-tree)
