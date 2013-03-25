289c27c0eb5221db8a85892cd689cf91039e6c1f
mickykebe <mickykebe@gmail.com>
2013-03-25 08:55:03 +0300

    2.63 a) updated

--- chapter2/2.63.scm
+++ chapter2/2.63.scm
@@ -18,4 +18,5 @@
 ;a) They always give the same result. 
 ;   For all 3 figures in figure set 2.16 both procedures give the result (1 3 5 7 9 11)
 
-;b) Both procedures grow at a rate of O(N)
+;b)  Although the first procedure looks to have O(N) complexity the call to append, which is an O(N) procedure, at each step of iteration increases the running time. At a glance, an O(N) procedure within a procedure of O(N) iteration looks to have a complexity of O(N^2), but a closer look shows that as tree->list1 moves from one iteration to the next, the number of elements decreases by half. And such O(NlogN) is the answer.
+;    Both procedures grow at a rate of O(N)
