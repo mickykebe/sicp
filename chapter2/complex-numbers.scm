dd1546671ae4ee0cb1813638259195e685b0aa85
mickykebe <mickykebe@gmail.com>
2013-03-27 13:47:50 +0300

    complex numbers

---
+++ chapter2/complex-numbers.scm
@@ -0,0 +1,24 @@
+; Ben's representation
+
+(define (real-part z) (car z))
+(define (imag-part z) (cdr z))
+(define (magnitude z)
+  (sqrt (+ (square (real-part z)) (square (imag-part z)))))
+(define (angle z)
+  (atan (imag-part z) (real-part z)))
+(define (make-from-real-imag x y) (cons x y))
+(define (make-from-mag-ang r a) 
+  (cons (* r (cos a)) (* r (sin a))))
+
+;Alyssa's representation
+
+(define (real-part z)
+  (* (magnitude z) (cos (angle z))))
+(define (imag-part z)
+  (* (magnitude z) (sin (angle z))))
+(define (magnitude z) (car z))
+(define (angle z) (cdr z))
+(define (make-from-real-imag x y) 
+  (cons (sqrt (+ (square x) (square y)))
+        (atan y x)))
+(define (make-from-mag-ang r a) (cons r a))
