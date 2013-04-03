672617038b67b02c58ccc998306e36173ff1a24f
mickykebe <mickykebe@gmail.com>
2013-04-03 10:57:00 +0300

    2.82 updated

--- chapter2/2.82.scm
+++ chapter2/2.82.scm
@@ -19,3 +19,8 @@
                         (apply apply-generic op (map (car valid-coerc-procs) args))))
               (error "No method for these types"
                      (list op type-tags)))))))
+
+; Situation where the above procedure isn't general enough:
+;   If for instance a subset of the arguments are convertable to one type and and the rest to another type, where the converts themselves can be converted to one distinct type.
+
+;In general observing the data in a heirarchical format. The coercion logic of the procedure works only on those types whose immediate supertype is the same. If however the heirarchy were to be extended up where a subset of the arguments have one supertype and the remaining subset another, where both supertypes have the same supertypes, the procedure proves inadequate.
