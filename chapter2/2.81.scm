925d20c553c1ef4af0c96167c36858d2d244c03a
mickykebe <mickykebe@gmail.com>
2013-04-03 08:41:00 +0300

    2.81

---
+++ chapter2/2.81.scm
@@ -0,0 +1,26 @@
+;a) an endless loop
+;   Had there been an operation defined for complex numbers the procedure, upon execution, wouldn't reach the coercion logic.
+;   However with no such operation, apply-generic keeps on recursing endlessly with one number being constantly coerced.
+;b) Coercion with arguments of the same type is not necessary because unless an operation is defined coercing one number is nothing more than trying to execute the operation over and over again, when the operation is not defined resulting in an endless loop
+;c)
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (and (= (length args) 2) (not (= (car type-tags) (cadr type-tags))))
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                         (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                         (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                         (error "No method for these types"
+                                (list op type-tags))))))
+              (error "No method for these types"
+                     (list op type-tags)))))))
