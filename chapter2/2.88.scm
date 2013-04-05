faea80e1a440ea47e779f4cfa9de7589324280a6
mickykebe <mickykebe@gmail.com>
2013-04-05 09:30:39 +0300

    2.88 updated

--- chapter2/2.88.scm
+++ chapter2/2.88.scm
@@ -2,7 +2,10 @@
     (apply-generic 'negate x))
 
 (define (sub-poly p1 p2)
-    (add-poly p1 (negate p2)))
+    (if (same-variable? (variable p1) (variable p2))
+        (add-poly p1 (negate p2))
+        (error "Polys not in same var -- ADD-POLY"
+               (list p1 p2))))
 
 (put 'sub '(polynomial polynomial)
      (lambda (p1 p2) (tag (sub-poly p1 p2))))
