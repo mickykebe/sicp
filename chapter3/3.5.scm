012864a4ea3b92a94b6e4658b0fc735dcd9a5fbb
mickykebe <mickykebe@gmail.com>
2013-04-11 16:12:37 +0300

    3.5

---
+++ chapter3/3.5.scm
@@ -0,0 +1,23 @@
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+(define (estimate-integral p x1 x2 y1 y2 trials)
+    (define (test)
+        (let ((x (random-in-range x1 x2))
+              (y (random-in-range y1 y2)))
+            (p x y)))
+    (define (area)
+        (lambda (x1 x2 y1 y2)
+            (* (- x2 x1) (- y2 y1))))
+    (* ((area) x1 x2 y1 y2) (monte-carlo trials test)))
