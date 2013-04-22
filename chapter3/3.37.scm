4a1a68e86f5acbf88905ae0d3a91f2bdf3fb2d56
mickykebe <mickykebe@gmail.com>
2013-04-22 16:35:55 +0300

    3.37

---
+++ chapter3/3.37.scm
@@ -0,0 +1,24 @@
+(define (celsius-fahrenheit-converter x)
+  (c+ (c* (c/ (cv 9) (cv 5))
+          x)
+      (cv 32)))
+
+(define (c+ x y)
+  (let ((z (make-connector)))
+    (adder x y z)
+    z))
+
+(define (c* x y)
+    (let ((z (make-connector)))
+        (multiplier x y z)
+        z))
+
+(define (c/ x y)
+    (let ((z (make-connector)))
+        (multiplier z y x)
+        z))
+
+(define (cv val)
+    (let ((z (make-connector)))
+        (constant val z)
+        z))
