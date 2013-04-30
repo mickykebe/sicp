fa569a38e78e9e43d5db82ac9e22db02d7b9f67a
mickykebe <mickykebe@gmail.com>
2013-04-30 16:35:39 +0300

    3.60

---
+++ chapter3/3.60.scm
@@ -0,0 +1,5 @@
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) 
+                  (stream-car s2)) 
+               (add-streams (mul-series (stream-cdr s1) s2)
+                            (mul-series s1 (stream-cdr s2)))))
