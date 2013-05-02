e00c2383d6d3804a8f96f483874d47ff3ebbfd1c
mickykebe <mickykebe@gmail.com>
2013-05-02 11:15:42 +0300

    3.57 updated

--- chapter3/3.57.scm
+++ chapter3/3.57.scm
@@ -8,10 +8,10 @@
                                          fibs))))
 
 ;With memoization in place:
-;When computing fib for n=2, add-streams computes fibs for n=0 and n=1 in which case (add-strems [fib 0] [fib 1]) is memoized.
-;When computing fib for n=3, add-streams computes fibs for n=1 and n=2 in which case (add-strems [fib 1] [fib 2]) is memoized.
+;When computing fib for n=2, add-streams computes fibs for n=0 and n=1 in which case (add-streams [fib 0] [fib 1]) is memoized.
+;When computing fib for n=3, add-streams computes fibs for n=1 and n=2 in which case (add-streams [fib 1] [fib 2]) is memoized.
 ;(But notice here that [fib 2] doesn't spawn another level of execution since it's result is memoized.)
-;When computing fib for n=4, add-streams computes fibs for n=2 and n=3 in which case (add-strems [fib 2] [fib 3]) is memoized.
+;When computing fib for n=4, add-streams computes fibs for n=2 and n=3 in which case (add-streams [fib 2] [fib 3]) is memoized.
 ;(Notice again the result for [fib 2] and [fib 3] is also pre-computed)
 ;Therefore for any value of n, the level of computation is a linear one.
 
