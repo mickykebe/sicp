ab1ff0a41ecb5c1f3f78cd8278097539b703de58
mickykebe <mickykebe@gmail.com>
2013-05-03 23:23:03 +0300

    3.66

---
+++ chapter3/3.66.scm
@@ -0,0 +1,8 @@
+;For (x,y) where x=1(1,y) the formulay 2y-2 suffices.
+;(1,100) = 198
+;It gets more complicated when x is a higher number.
+;A general observation would be:
+;(1,val) appears 2 iterations apart with val exceeding the previous val by one.
+;(2,val) appears 4 iterations apart with val exeeding the previous one by one.
+;(3,val) >>      8 >>              >>       >>      >>              >>       .
+;Formula -> I give up.
