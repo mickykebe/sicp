7e244074804cf895f859ada88c41ff432c724430
mickykebe <mickykebe@gmail.com>
2013-05-06 11:06:20 +0300

    3.70 updated

--- chapter3/3.70.scm
+++ chapter3/3.70.scm
@@ -19,7 +19,7 @@
         (merge-weighted
             (stream-map (lambda (x) (list (stream-car s) x))
                         (stream-cdr t))
-            (pairs (stream-cdr s) (stream-cdr t))
+            (weighted-pairs (stream-cdr s) (stream-cdr t))
             weight-proc)))
 
 ;a
