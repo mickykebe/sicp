b554b7da970a3782bb5c52feaee2c2ea39a1f83c
mickykebe <mickykebe@gmail.com>
2013-05-07 11:17:42 +0300

    evaluation delayed streams

---
+++ chapter3/evaluation-delayed-streams.scm
@@ -0,0 +1,20 @@
+(define int
+  (cons-stream initial-value
+               (add-streams (scale-stream integrand dt)
+                            int)))
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (let ((integrand (force delayed-integrand)))
+                   (add-streams (scale-stream integrand dt)
+                                int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(stream-ref (solve (lambda (y) y) 1 0.001) 1000)
+;2.716923932235896
