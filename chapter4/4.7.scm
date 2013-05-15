9dbd80aefea9e3869d1b065528604f46e7e372ae
mickykebe <mickykebe@gmail.com>
2013-05-15 11:21:44 +0300

    4.7 tested

--- chapter4/4.7.scm
+++ chapter4/4.7.scm
@@ -1,15 +1,15 @@
 (define (let*? exp) (tagged-list? exp 'let*))
-(define (let-var-exp-seq exp) (cadr exp))
-(define (let-body exp) (cddr exp))
-(define (first-var-exp var-exp-seq) (car var-exp-seq))
-(define (last-var-exp? var-exp-seq) (null? (cdr var-exp-seq)))
+(define (let*-var-exp-seq exp) (cadr exp))
+(define (let*-body exp) (cddr exp))
+(define (let*-first-var-exp var-exp-seq) (car var-exp-seq))
+(define (let*-last-var-exp? var-exp-seq) (null? (cdr var-exp-seq)))
 
 (define (let*->nested-lets exp)
     (define (transform-iter var-exp-seq)
-        (if (last-var-exp? var-exp-seq)
-            (make-let (list (first-var-exp var-exp-seq)) (let-body exp))
-            (make-let (list (first-var-exp var-exp-seq)) (transform-iter (cdr var-exp-seq)))))
-    (transform-iter (let-var-exp-seq exp)))
+        (if (let*-last-var-exp? var-exp-seq)
+            (make-let (list (let*-first-var-exp var-exp-seq)) (sequence->exp (let*-body exp)))
+            (make-let (list (let*-first-var-exp var-exp-seq)) (transform-iter (cdr var-exp-seq)))))
+    (transform-iter (let*-var-exp-seq exp)))
 
 ;yes
 ;to be added to eval
