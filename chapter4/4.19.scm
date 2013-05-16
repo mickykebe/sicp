41479014dd8c5a2aac2b0dcc9190f519266122ca
mickykebe <mickykebe@gmail.com>
2013-05-16 11:00:55 +0300

    4.19

---
+++ chapter4/4.19.scm
@@ -0,0 +1,3 @@
+;Eva's.(The footnote has the answer)
+;Devising a way that works as eva proposed is difficult. Instead of doing scoping out sequentially with the procedures defined, it would mean we'd have to analyze each expression if it uses a variable defined in the same scope and postpone it's evaluation after the definitions it relies on have been evaluated. Not to mention recursive definitions that would further complicate the problem.
+;As the footnote states, Alyssa's version might be the most practical.
