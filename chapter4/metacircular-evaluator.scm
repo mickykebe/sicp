dc02f2892dd5079a924e364e2c52e88fc5e92218
mickykebe <mickykebe@gmail.com>
2013-05-19 09:12:05 +0300

    metacircular evaluator

--- chapter4/metacircular-evaluator.scm
+++ chapter4/metacircular-evaluator.scm
@@ -13,6 +13,19 @@
         ((begin? exp) 
          (eval-sequence (begin-actions exp) env))
         ((cond? exp) (eval (cond->if exp) env))
+        ;-----------ex4.4------------------------
+        ((and? exp) (eval-and (and-operands exp) env))
+        ((or? exp) (eval-or (or-operands exp) env))
+        ;----------------------------------------
+        ;-----------ex4.6------------------------
+        ((let? exp) (eval (let->combination exp) env))
+        ;----------------------------------------
+        ;-----------ex4.7------------------------
+        ((let*? exp) (eval (let*->nested-lets exp) env))
+        ;----------------------------------------
+        ;-----------ex4.20------------------------
+        ((letrec? exp) (eval (letrec->combination exp) env))
+        ;-----------------------------------------
         ((application? exp)
          (apply (eval (operator exp) env)
                 (list-of-values (operands exp) env)))
@@ -135,7 +148,7 @@
 (define (cond-else-clause? clause)
   (eq? (cond-predicate clause) 'else))
 (define (cond-predicate clause) (car clause))
-(define (cond-actions clause) (cdr clause))
+(define (cond-actions clause) (cdr clause)) ;overriden by exercise 4.5
 (define (cond->if exp)
   (expand-clauses (cond-clauses exp)))
 
@@ -152,6 +165,104 @@
             (make-if (cond-predicate first)
                      (sequence->exp (cond-actions first))
                      (expand-clauses rest))))))
+
+;Exercise additions
+;----------4.4----------
+;and
+(define (and? exp)
+    (tagged-list? exp 'and))
+(define (and-operands exp) (cdr exp))
+(define (no-and-operands? ops) (null? ops))
+(define (first-and-operand ops) (car ops))
+(define (rest-and-operands ops) (cdr ops))
+(define (eval-and ops env)
+    (cond ((no-and-operands? ops) true)
+          ((false? (eval (first-and-operand ops) env)) false)
+          (else (eval-and (rest-and-operands ops) env))))
+
+;or
+(define (or? exp)
+    (tagged-list? exp 'or))
+(define (or-operands exp) (cdr exp))
+(define (no-or-operands? ops) (null? ops))
+(define (first-or-operand ops) (car ops))
+(define (rest-or-operands ops) (cdr ops))
+(define (eval-or ops env)
+    (cond ((no-or-operands? ops) false)
+          ((true? (eval (first-or-operand ops) env)) true)
+          (else (eval-or (rest-or-operands ops) env))))
+
+;-------------4.5------------
+(define (cond-actions clause) 
+    (if (eq? (cadr clause) '=>)
+        (list (caddr clause) (car clause))
+        (cdr clause)))
+
+;-------------4.6------------
+(define (let? exp)
+    (tagged-list? exp 'let))
+(define (let->combination exp) ;overriden by ex 4.8 to accomodate named-let
+    (cons (make-lambda (let-vars exp) 
+                       (let-body exp))
+            (let-exps exp)))
+(define (let-vars exp)
+    (map car (cadr exp)))
+(define (let-exps exp)
+    (map cadr (cadr exp)))
+(define (let-body exp)
+    (cddr exp))
+(define (make-let var-exps body)
+    (list 'let var-exps body))
+
+;-------------4.7------------
+(define (let*? exp) (tagged-list? exp 'let*))
+(define (let*-var-exp-seq exp) (cadr exp))
+(define (let*-body exp) (cddr exp))
+(define (let*-first-var-exp var-exp-seq) (car var-exp-seq))
+(define (let*-last-var-exp? var-exp-seq) (null? (cdr var-exp-seq)))
+
+(define (let*->nested-lets exp)
+    (define (transform-iter var-exp-seq)
+        (if (let*-last-var-exp? var-exp-seq)
+            (make-let (list (let*-first-var-exp var-exp-seq)) (sequence->exp (let*-body exp)))
+            (make-let (list (let*-first-var-exp var-exp-seq)) (transform-iter (cdr var-exp-seq)))))
+    (transform-iter (let*-var-exp-seq exp)))
+
+;-------------4.8------------
+(define (make-define var exp) (list 'define var exp))
+
+(define (named-let? exp) (symbol? (named-let-name exp)))
+(define (named-let-name exp) (cadr exp))
+(define (named-let-vars exp) (map car (caddr exp)))
+(define (named-let-exps exp) (map cadr (caddr exp)))
+(define (named-let-body exp) (cdddr exp))
+
+(define (let->combination exp)
+    (if (named-let? exp)
+        (sequence->exp (list (make-define (named-let-name exp)
+                                          (make-lambda (named-let-vars exp)
+                                                       (named-let-body exp)))
+                             (cons (named-let-name exp) (named-let-exps exp))))
+        (cons (make-lambda (let-vars exp) 
+                           (let-body exp))
+              (let-exps exp))))
+
+;-------------4.20-----------
+(define (letrec? exp) (tagged-list? exp 'letrec))
+
+(define (letrec-var-exp-seq exp) (cadr exp))
+(define (letrec-body exp) (cddr exp))
+
+(define (letrec->combination exp)
+    (define (make-var-bod-seq var-exp-seq)
+        (map (lambda (var-exp)
+                (cons (list (car var-exp) ''*unassigned)
+                      (list 'set! (car var-exp) (cadr var-exp))))
+             var-exp-seq))
+    (let ((seq (make-var-bod-seq (letrec-var-exp-seq exp))))
+        (make-let (map car seq)
+                  (sequence->exp (append (map cdr seq)
+                                         (letrec-body exp))))))
 
 ;Evaluator data structures
 (define (true? x)
@@ -237,6 +348,8 @@
         (list '* *)
         (list '- -)
         (list '/ /)
+        (list '= =)
+        (list 'display display)
         ;<more primitives>
         ))
 (define (primitive-procedure-names)
