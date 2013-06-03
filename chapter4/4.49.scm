25e6ee28c4faca7a30a445c6ca16273d1e81871c
mickykebe <mickykebe@gmail.com>
2013-06-03 17:32:34 +0300

    4.49 corrected

--- chapter4/4.49.scm
+++ chapter4/4.49.scm
@@ -1,2 +1,14 @@
 (define (parse-word word-list)
-    (list (car word-list) (an-element-of (cdr word-list))))
+  (require (not (null? *unparsed*)))
+  (let ((found-word (an-element-of (cdr word-list))))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+;Results
+;(sentence (simple-noun-phrase (article the) (noun student)) (verb studies))
+;(sentence (simple-noun-phrase (article the) (noun student)) (verb lectures))
+;(sentence (simple-noun-phrase (article the) (noun student)) (verb eats))
+;(sentence (simple-noun-phrase (article the) (noun student)) (verb sleeps))
+;(sentence (simple-noun-phrase (article the) (noun professor)) (verb studies))
+;(sentence (simple-noun-phrase (article the) (noun professor)) (verb lectures))
+;(sentence (simple-noun-phrase (article the) (noun professor)) (verb eats))
