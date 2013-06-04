15486b3ec9740c2f37b186500467a2d81e5d8972
mickykebe <mickykebe@gmail.com>
2013-06-04 09:48:45 +0300

    4.53

---
+++ chapter4/4.53.scm
@@ -0,0 +1,13 @@
+(define (prime-sum-pair list1 list2)
+  (let ((a (an-element-of list1))
+        (b (an-element-of list2)))
+    (require (prime? (+ a b)))
+    (list a b)))
+
+(let ((pairs '()))
+  (if-fail (let ((p (prime-sum-pair '(1 3 5 8) '(20 35 110))))
+             (permanent-set! pairs (cons p pairs))
+             (amb))
+           pairs))
+
+;((8 35) (3 110) (3 20))
