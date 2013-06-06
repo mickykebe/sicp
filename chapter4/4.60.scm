fcf308ddec23f4ecbb0fd264352f6ea73422e17b
mickykebe <mickykebe@gmail.com>
2013-06-06 11:12:24 +0300

    4.60

---
+++ chapter4/4.60.scm
@@ -0,0 +1,2 @@
+;Happens because we don't have the foresight of knowing what's already been matched while matching a query with a pattern in a data base.
+;A possible solution could be if we accumulate the results and use them as a data base to build a simple rule that fixes the problem.
