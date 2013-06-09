cc6de76d1fff9c5cb05dc5e5ee33d4a669d8229f
mickykebe <mickykebe@gmail.com>
2013-06-09 10:52:33 +0300

    4.66

---
+++ chapter4/4.66.scm
@@ -0,0 +1,2 @@
+;Ben's system won't work because as Cy has showed a pattern variable with its result can be present more than once at the end of evaluation.
+;One possible solution is if instead of passing a vriable to the accumulation function to have a procedure the user crafts to filter the results.
