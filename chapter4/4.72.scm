030d42824f2099e208118d083580a52a13a26bb7
mickykebe <mickykebe@gmail.com>
2013-06-10 13:47:35 +0300

    4.72

---
+++ chapter4/4.72.scm
@@ -0,0 +1,3 @@
+;When dealing with queries whose execution results in infinite streams such interleaving makes sense.
+;For instance evaluating an "or" clause with two patterns, the first under evaluation culminating in an infinite stream of results and the second having a fixed number of result frames, appending the two streams, in the given order, would have been impossible.
+;Interleaving is the ideal solution for this scenario because despite the infinite nature of the first stream, we can now observe the evaluation results of both clauses.
